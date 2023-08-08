1eb0a691ee6532dd460b6f381e1e9837adf9053d
brent <d.brent.marshall@gmail.com>
2023-08-08 17:06:03 -0400

    factor out config reader

---
+++ RatatouilleWebsite/configReader.js
@@ -0,0 +1,33 @@
+ 	
+  		{
+  			var courseConfig;
+  			var stageNumber;
+  		}
+  					
+		async function readConfigFromQueryString(callback) {
+			console.log("readConfigFromQueryString");
+		
+			// read REST parameters
+			const queryString = window.location.search;
+			const urlParms = new URLSearchParams(queryString);
+
+			// read course config file name and stage to execute
+			const configFileName = urlParms.get("config");
+			console.log("configFileName: "+configFileName);
+			stageNumber = urlParms.get("stage");
+		
+			let url = "./" + configFileName;
+			
+			let response = await fetch(url);
+	
+			if (response.ok) { // if HTTP-status is 200-299
+				courseConfig = await response.json();
+			} else {
+				alert("HTTP Error while retrieving the course configuration.\n" + response.status);
+			}
+			
+			console.log("JSON from config file:\n");
+			console.log(courseConfig);
+			
+			callback();
+		}
