89cf0816baa0b1459bfe77043cd1135f8ff99373
brent <d.brent.marshall@gmail.com>
2023-08-11 11:34:34 -0400

    checkmark overlay

--- RatatouilleWebsite/testRunner.js
+++ RatatouilleWebsite/testRunner.js
@@ -2,10 +2,9 @@
 
 {
 	var currentQuestionIndex = 0;
-	var stageNumber;
-	var courseConfig;
 	var testConfig;
-	var contentArea= document.getElementById("contentArea");
+	var contentArea = document.getElementById("contentArea");
+	var questionDiv = null;
 }
 
 function displayCurrentQuestion() {
@@ -22,25 +21,30 @@ function displayCurrentQuestion() {
 	outerDiv.width = '100%';
 
 	// display question
+	questionDiv = document.createElement('div');
+	questionDiv.classList.add("question-container");
+	outerDiv.appendChild(questionDiv);
+	
 	const question = document.createElement("p");
 	question.classList.add("question");
 	question.innerText = currentQuestion.question;
-	outerDiv.appendChild(question);
+	questionDiv.appendChild(question);
 	
 	// create form for answers
 	const answerForm = document.createElement("form");
-	answerForm.classList.add("question-container");
-	outerDiv.appendChild(answerForm);
+	answerForm.classList.add('answer-container');
+	questionDiv.appendChild(answerForm);
 	
 	// display answers
 	for (var answerIndex in currentQuestion.answers) {
 		var answer = currentQuestion.answers[answerIndex];
 		
 		// create elements for the answer
-		const control = document.createElement('input');
+		var control = document.createElement('input');
 		control.type = currentQuestion.controlType;
+		control.checked = answer.correctChoice;
 		
-		const label = document.createElement('label');
+		var label = document.createElement('label');
 		label.classList.add("answer");
 		label.innerText = answer.text;
 		
@@ -54,17 +58,65 @@ function displayCurrentQuestion() {
 		
 		// break
 		answerForm.appendChild( document.createElement('br') );
+		
+		// associate answer with view
+		answer.controlView = control;
 	}
 	
 	var buttonDiv = document.createElement('div');
 	
 	var submitButton = document.createElement('button');
 	submitButton.innerText = "Submit";
+	submitButton.onclick = evaluateResponse;
 	buttonDiv.appendChild(submitButton);
 	
 	outerDiv.appendChild(buttonDiv);
 	
 	contentArea.appendChild(outerDiv);
+}
+
+function displayCheckOrX(check) {
+	var imageFile;
+	if (check) {
+		imageFile = './assets/checkMark.png';
+	} else {
+		imageFile = './assets/X.png';
+	}
+	
+// 	var image = document.createElement('div');
+	var image = new Image();
+	
+// 	image.onload = (event) => {
+// 		console.log('image loaded ...');
+// 	}
+	image.src = imageFile;
+ 	image.classList.add('checkOrX');
+	
+	var div = document.createElement('div');
+	div.classList.add('overlayPanel');
+	div.appendChild(image);
+	
+// 	contentArea.innerHTML = '';
+	questionDiv.appendChild(div);
+}
+
+function evaluateResponse() {
+	console.log("evaluateResponse");
+	
+	var correct = true;
+	for (var answerIndex in testConfig.questions[currentQuestionIndex].answers) {
+		var currentAnswer = testConfig.questions[currentQuestionIndex].answers[answerIndex];
+				
+		correct = correct && (currentAnswer.correctChoice === currentAnswer.controlView.checked);
+	}
+	
+	if (correct) {
+		console.log("correct");
+	} else {
+		console.log("wrong");
+	}
+	
+	displayCheckOrX(correct);
 }
 
 async function readTestQuestions(fileName, callback) {
